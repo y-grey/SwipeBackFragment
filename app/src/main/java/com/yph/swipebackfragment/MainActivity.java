19a22daf64caa894dc643831eaa4cdc142b627f4
YPH <542391099@qq.com>
2016-04-24 14:22:12 +0800

    update

--- app/src/main/java/com/yph/swipebackfragment/MainActivity.java
+++ app/src/main/java/com/yph/swipebackfragment/MainActivity.java
@@ -4,8 +4,6 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -16,23 +14,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
     public void addFragmentClick(View v){
         Toast.makeText(this,"!!",Toast.LENGTH_SHORT).show();
